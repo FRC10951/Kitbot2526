faca83b25e2beb2bfd19dfd368f36b5f30177bb9
znotfireman <znotfireman@gmail.com>
2026-02-09 16:16:46 +0700

    feat: fix left dt ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,7 +17,7 @@ package frc.robot;
 public final class Constants {
   public static final class DriveConstants {
     // Motor controller IDs for drivetrain motors
-    public static final int LEFT_LEADER_ID = 4;
+    public static final int LEFT_LEADER_ID = 11;
     public static final int LEFT_FOLLOWER_ID = 8;
     public static final int RIGHT_LEADER_ID = 10;
     public static final int RIGHT_FOLLOWER_ID = 7;
